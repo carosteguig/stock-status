7705f419ca43a745c3ad7735c0630bd3d1316360
carosteguig <carosteguig@gmail.com>
2022-05-17 20:23:47 -0700

    added card  components to home page and created .env file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import './Home.scss';
+import React from 'react';
 import { Component } from 'react';
 import Card from '../../components/Card/Card';
+import axios from 'axios';
+import Card from '../../components/Card/Card';
+
 
 export default class Home extends Component {
 
     state = {
-    paintArr: []
+    paints: []
 }
 
 
@@ -19,24 +23,24 @@ export default class Home extends Component {
                 <h1 className='main__title'>Paint Stock</h1>
                 <section cardName='main__cards'>
                     <Card 
-                       key='' 
-                       title=''
-                       items=''
-                       quantity=''
+                       key='1' 
+                       title='Available'
+                       items='blue'
+                       quantity='100'
 
                     />
                    <Card 
-                       key='' 
-                       title=''
-                       items=''
-                       quantity=''
+                       key='2' 
+                       title='Running Low'
+                       items='reed'
+                       quantity='9'
 
                     />
                     <Card 
-                       key='' 
-                       title=''
-                       items=''
-                       quantity=''
+                       key='3' 
+                       title='Out of Stock'
+                       items='black'
+                       quantity='0'
 
                     />
                 </section>
